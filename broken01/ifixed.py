f1cbe01fee799995b42eb360d130af21ca0719e8
Jorge Torres <jorge.torres4@verizonwireless.com>
2018-12-12 14:12:19 -0500

    adjustment to lab 19

--- broken01/ifixed.py
+++ broken01/ifixed.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # A program that prompts a user for two operators and and operation (plus or minus)
 # the program then shows the result.
 # The user may enter 'q' to exit the program.
@@ -8,22 +8,22 @@ calc3 = 0.0
 operation = ''
 while (calc1 != 'q'):
     print('\nWhat is the first operator? Or, enter q to quit: ')
-    calc1 = raw_input()
+    calc1 = input()
     if calc1.lower () == 'q':
         break
     calc1 = float(calc1)
     print('\nWhat is the second operator? Or, enter q to quit: ')
-    calc2 = raw_input()
+    calc2 = input()
     if calc2.lower () == 'q':
         break
     calc2 = float(calc2)
     print('\nWhat is the third operator? Or, enter q to quit: ')
-    calc3 = raw_input()
+    calc3 = input()
     if calc3.lower () == 'q':
         break
     calc3 = float(calc3)
     print('Enter an operation to perform on the two operators (+ or -): ')
-    operation = raw_input()
+    operation = input()
     if operation == '+':
         print('\n' + str(calc1) + ' + ' + str(calc2) + ' + ' + str(calc3) + ' = ' + str(calc1 + calc2 + calc3))
     elif operation == '-':
